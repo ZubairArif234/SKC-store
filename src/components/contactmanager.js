2f88ccb254b93f2a89cde2844ffe6fbb4b6622e2
zubair arif <zubarif234@gmail.com>
2023-03-03 22:01:44 +0500

    about component added

--- src/components/contactmanager.js
+++ src/components/contactmanager.js
@@ -1,26 +1,26 @@
 
 
 
-// import './index.css';
+import './index.css';
 
 
 
-// function ContactManager() {
-//   return (
-//     <div className="headertags" style={{backgroundColor:'black', padding:'5px', margin:'20px',marginTop:'50px'}}>
-//         <h1 className='aboutheading'>CONTACT</h1>
-//         <h4 className='aboutheading'>The Management</h4>
-//       <p className='abouttext' >SKC STORE is providing bset setvice to their customers. You get get in touch with the Management via Instagram.</p>
-//       <p className='aboutheading'>FEEL FREE TO ASK ANY QUERY !</p>
-//       <center>
+function ContactManager() {
+  return (
+    <div className="headertags" style={{backgroundColor:'black', padding:'5px', margin:'20px',marginTop:'50px'}}>
+        <h1 className='aboutheading'>CONTACT</h1>
+        <h4 className='aboutheading'>The Management</h4>
+      <p className='abouttext' >SKC STORE is providing bset setvice to their customers. You get get in touch with the Management via Instagram.</p>
+      <p className='aboutheading'>FEEL FREE TO ASK ANY QUERY !</p>
+      <center>
 
-//       <button className='contactmanagerbtn'>
-//         <a className='skcinstalink' href='https://www.instagram.com/skcs_studio/?igshid=YmMyMTA2M2Y%3D'>skcs_studio</a></button>
-//       </center>
+      <button className='contactmanagerbtn'>
+        <a className='skcinstalink' href='https://www.instagram.com/skcs_studio/?igshid=YmMyMTA2M2Y%3D'>skcs_studio</a></button>
+      </center>
       
      
-//     </div>
-//   );
-// }
+    </div>
+  );
+}
 
-// export default ContactManager;
+export default ContactManager;
