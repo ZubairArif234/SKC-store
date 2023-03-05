c063e231938b87f9766b051a0edfdbb907ad0c60
zubair arif <zubarif234@gmail.com>
2023-03-05 11:38:00 +0500

    contact to dev added

--- src/components/contactdeveloper.js
+++ src/components/contactdeveloper.js
@@ -3,26 +3,26 @@
 
 
 
-// import './index.css';
+import './index.css';
 
 
 
-// function ContactDeveloper() {
-//   return (
-//     <div className="headertags" style={{backgroundColor:'gray', padding:'5px', margin:'20px',marginTop:'50px'}}>
-//         <h1 className='aboutheading'>WANT YOUR OWN WEBSITE</h1>
-//         <h4 className='aboutheading'> Contact The Developer</h4>
-//       <p className='abouttext' >Get your desired website at the resonable price to start your bussiness.</p>
-//       <p className='aboutheading'>Contact me now via Instagram</p>
-//       <center>
+function ContactDeveloper() {
+  return (
+    <div className="headertags" style={{backgroundColor:'gray', padding:'5px', margin:'20px',marginTop:'50px'}}>
+        <h1 className='aboutheading'>WANT YOUR OWN WEBSITE</h1>
+        <h4 className='aboutheading'> Contact The Developer</h4>
+      <p className='abouttext' >Get your desired website at the resonable price to start your bussiness.</p>
+      <p className='aboutheading'>Contact me now via Instagram</p>
+      <center>
 
-//       <button className='contactmanagerbtn'>
-//         <a className='skcinstalink' href='https://www.instagram.com/zubairarif234/'>CONATACT DEVELOPER</a></button>
-//       </center>
+      <button className='contactmanagerbtn'>
+        <a className='skcinstalink' href='https://www.instagram.com/zubairarif234/'>CONATACT DEVELOPER</a></button>
+      </center>
       
      
-//     </div>
-//   );
-// }
+    </div>
+  );
+}
 
-// export default ContactDeveloper;
+export default ContactDeveloper;
